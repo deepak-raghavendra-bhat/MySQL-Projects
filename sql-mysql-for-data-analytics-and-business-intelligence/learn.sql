b13bdca59888194a930f25b24b74e2c4db3d6d0c
deepak.raghavendra.bhat <deepak.raghavendra.bhat@outlook.com>
2023-04-12 14:17:58 +0530

    SQL course materials

---
+++ sql-mysql-for-data-analytics-and-business-intelligence/learn.sql
@@ -0,0 +1,10 @@
+-- Active: 1681279878528@@127.0.0.1@3306
+show DATABASEs;
+
+create database if not exists demo_table;
+
+show databases;
+
+create schema if not exists demo_schema;
+
+use demo_table;
